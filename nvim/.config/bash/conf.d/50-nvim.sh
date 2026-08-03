bbc1a41fdb0e5ea3f9b537d765a3c303eb24560d
Andrey Latipov <dark.vvorth@gmail.com>
2026-08-03 13:45:50 +0500

    fix broken nvim-less replacement

--- nvim/.config/bash/conf.d/50-nvim.sh
+++ nvim/.config/bash/conf.d/50-nvim.sh
@@ -3,9 +3,9 @@
 alias vi=nvim
 alias vim=nvim
 
-NVIM_LESS=/usr/share/nvim/runtime/scripts/less.sh
-[[ -f $NVIM_LESS ]] && alias less=$NVIM_LESS || true
-[[ -f $NVIM_LESS ]] && PAGER=$NVIM_LESS || true
+#NVIM_LESS=/usr/share/nvim/runtime/scripts/less.sh
+#[[ -f $NVIM_LESS ]] && alias less=$NVIM_LESS || true
+#[[ -f $NVIM_LESS ]] && PAGER=$NVIM_LESS || true
 
 export EDITOR=nvim
 export MANPAGER='nvim +Man!'
