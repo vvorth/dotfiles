72780b9ca1b324a1fbf423a4b2d8ce3d55f2a808
Andrey Latipov <dark.vvorth@gmail.com>
2026-08-05 16:19:36 +0500

    reintroduce iterm integration in branch

---
+++ iterm2-bash/.config/bash/conf.d/90-iterm2_shell_integration.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+[[ -f ~/.iterm2_shell_integration.bash ]] && . ~/.iterm2_shell_integration.bash || true
+
---
+++ iterm2-zsh/.config/zsh/conf.d/90-iterm2_shell_integration.zsh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+[[ -f ~/.iterm2_shell_integration.zsh ]] && source ~/.iterm2_shell_integration.zsh || true
+
